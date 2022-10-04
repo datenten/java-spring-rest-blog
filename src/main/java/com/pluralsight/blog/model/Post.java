bd9be1cecc980ead650a660b88940c098d92c6ce
@TienLe <viet-tien.le@andrena.de>
2022-10-04 17:26:19 +0200

    add versioning and validation

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -17,6 +17,11 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Version
+    private Long version;
+    @NotNull
+    @Size(min = 4, max = 100)
     private String title;
     @Column(length=1000000)
     @Lob
@@ -27,6 +32,8 @@ public class Post {
 
     @ManyToOne(fetch = FetchType.EAGER)
     private Author author;
+
+
 
     public Post() {
         super();
